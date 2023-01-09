9defa206d41a3a7a52a3cfc3167439a5e38d5e6f
Marcus Johnson <marcusjohnson88.mj@gmail.com>
2023-01-09 15:32:36 -0700

    sign up works now just need to add the auth side and fix the login portion. I also need to had the toggle to see password and figure out more about starting on diff servers

--- src/components/authentication/Login.jsx
+++ src/components/authentication/Login.jsx
@@ -4,13 +4,13 @@ import styles from "./Login.module.css";
 
 const Login = (props) => {
   return (
-    <div className={styles.login_container}>
+    <form className={styles.login_container}>
       <h2 className={styles.login_title}>Login</h2>
       <div className={styles.inner_top}>
         <label htmlFor="email">Email</label>
         <input id="email" type="text" placeholder=" Enter your email" />
         <label htmlFor="password">Password</label>
-        <input id="password" type="text" placeholder="Enter your password" />
+        <input id="password" type="password" placeholder="Enter your password" />
       </div>
       <div className={styles.inner_bottom}>
         <button className={styles.login_btn} id="login-btn">
@@ -21,11 +21,12 @@ const Login = (props) => {
           className={styles.signUp_btn}
           id="sign-up-screen"
           onClick={props.authScreenHandler}
+          type= "button"
         >
           Sign up
         </button>
       </div>
-    </div>
+    </form>
   );
 };
 
--- src/components/authentication/SignUp.jsx
+++ src/components/authentication/SignUp.jsx
@@ -1,53 +1,106 @@
-import React from "react";
+import React, { useState } from "react";
+
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 import styles from "./SignUp.module.css";
 
 const SignUp = (props) => {
+  const [email, setEmail] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const emailHandler = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const firstNameHandler = (e) => {
+    setFirstName(e.target.value);
+  };
+
+  const lastNameHandler = (e) => {
+    setLastName(e.target.value);
+  };
+
+  const passwordHandler = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    axios
+      .post("http://localhost:3008/register", {
+        firstName,
+        lastName,
+        email,
+        password,
+      })
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((error) => console.log(error, "error during sign up"));
+    navigate("/");
+    setEmail("");
+    setFirstName("");
+    setLastName("");
+    setPassword("");
+  };
+
   return (
-    <div>
-      <div className={styles.signUp_container}>
-        <h2 className={styles.signUp_title}>Sign Up</h2>
-        <div className={styles.inner_top}>
-          <label htmlFor="first-name">First name</label>
-          <input
-            id="register-first-name"
-            type="text"
-            placeholder=" Enter your first name"
-          />
-          <label htmlFor="last-name">Last name</label>
-          <input
-            id="register-last-name"
-            type="text"
-            placeholder="Enter your last name"
-          />
-          <label htmlFor="email">Email</label>
-          <input
-            id="register-email"
-            type="text"
-            placeholder="Enter your email"
-          />
-          <label htmlFor="password">Password</label>
-          <input
-            id="register-password"
-            type="text"
-            placeholder="Enter your password"
-          />
-        </div>
-        <div className={styles.inner_bottom}>
-          <button className={styles.signUp_btn} id="sign-up-btn">
-            Sign Up
-          </button>
-          <h4>Or</h4>
-          <button
-            className={styles.login_btn}
-            id="login-screen"
-            onClick={props.authScreenHandler}
-          >
-            Login
-          </button>
-        </div>
+    <form className={styles.signUp_container} onSubmit={submitHandler}>
+      <h2 className={styles.signUp_title}>Sign Up</h2>
+      <div className={styles.inner_top}>
+        <label htmlFor="first-name">First name</label>
+        <input
+          id="register-first-name"
+          type="text"
+          placeholder=" Enter your first name"
+          onChange={firstNameHandler}
+          value={firstName}
+        />
+        <label htmlFor="last-name">Last name</label>
+        <input
+          id="register-last-name"
+          type="text"
+          placeholder="Enter your last name"
+          onChange={lastNameHandler}
+          value={lastName}
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          id="register-email"
+          type="text"
+          placeholder="Enter your email"
+          onChange={emailHandler}
+          value={email}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          id="register-password"
+          type="password"
+          placeholder="Enter your password"
+          onChange={passwordHandler}
+          value={password}
+        />
       </div>
-    </div>
+      <div className={styles.inner_bottom}>
+        <button className={styles.signUp_btn} id="sign-up-btn">
+          Sign Up
+        </button>
+        <h4>Or</h4>
+        <button
+          className={styles.login_btn}
+          id="login-screen"
+          onClick={props.authScreenHandler}
+          type= "button"
+        >
+          Login
+        </button>
+      </div>
+    </form>
   );
 };
 
