193acb9b5e75333b70acba74d7ebd0b8ea5c6de9
Marcus Johnson <marcusjohnson88.mj@gmail.com>
2023-01-11 16:51:36 -0700

    add my banner and started working on my nav dropdown

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,25 +1,50 @@
-import {useState} from "react";
+import { useState } from "react";
+
+import { Link } from "react-router-dom";
 
 import styles from "./Header.module.css";
 import logo from "../../assets/logo.png";
 import "../UI/hamburger.css";
 
 const Header = () => {
-  const [isActive, setIsActive] = useState(false)
+  const [isActive, setIsActive] = useState(false);
   const toggleMenu = () => {
-    setIsActive(!isActive)
+    setIsActive(!isActive);
   };
-
 
   return (
     <header className={styles.header}>
       <img className={styles.logo} src={logo} alt="linked_logo" />
       <nav>
-        <button className={isActive ? "hamburger hamburger--spring is-active" : "hamburger hamburger--spring"} type="button" onClick={toggleMenu}>
+        <button
+          className={
+            isActive
+              ? "hamburger hamburger--spring is-active"
+              : "hamburger hamburger--spring"
+          }
+          type="button"
+          onClick={toggleMenu}
+        >
           <span className="hamburger-box">
             <span className="hamburger-inner"></span>
           </span>
         </button>
+        <div
+          className={!isActive ? styles.drop_down : styles.drop_down_displayed}
+        >
+          <Link to="/" className={styles.header_link}>
+            <h3>Home</h3>
+          </Link>
+          <Link to="/" className={styles.header_link}>
+            <h3>Add Cars</h3>
+          </Link>
+          <Link to="/settings" className={styles.header_link}>
+            <h3>Settings</h3>
+          </Link>
+          <Link to="/auth" className={styles.header_link}>
+            <button>Logout</button>
+          </Link>
+        </div>
       </nav>
     </header>
   );
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+
+import styles from "../components/UI/HomeScreen.module.css";
 
 const HomeScreen = () => {
   return (
-    <div>
-      
+    <div className={styles.main}>
+      <div className={styles.main_background}>
+        <h1>Welcome</h1>
+      </div>
+      <section></section>
     </div>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
