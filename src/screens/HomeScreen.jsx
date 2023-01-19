de68cd550b64654d7868e0296153f2365d5436eb
Marcus Johnson <marcusjohnson88.mj@gmail.com>
2023-01-18 20:05:50 -0700

    I think this is the end for now

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import styles from "../components/UI/HomeScreen.module.css";
+import logo from '../assets/logo.png'
 
 const HomeScreen = () => {
   return (
@@ -8,7 +9,25 @@ const HomeScreen = () => {
       <div className={styles.main_background}>
         <h1>Welcome</h1>
       </div>
-      <section></section>
+      <article className={styles.main_content}>
+        <h2>What is Linked?</h2>
+        <p>
+          Linked is an application dedicated to helping individuals stay up to
+          date and in the know on their vehicle's health. Tracking and alerting
+          users whenever something is wrong or due for inspection.
+        </p>
+          <h2>Future Addtions</h2>
+        <div className={styles.main_content_bottom}>
+          <p>Registration with your state's DMV</p>
+          <p>
+            Live Link through moblie app to your car's ECU (Engine Control Unit)
+          </p>
+          <p>Live Alerts and Notifications</p>
+        </div>
+      </article>
+      <div className={styles.bottom_logo_container}>
+        <img src={logo} alt="linked_logo" className={styles.logo_bottom} />
+      </div>
     </div>
   );
 };
